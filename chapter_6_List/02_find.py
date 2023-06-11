03a4035046bc169792439f2fc5cb2a44aaabca4e
jian899 <mjian528@gmail.com>
2023-06-11 20:25:25 +0800

    Add files via upload

---
+++ chapter_6_List/02_find.py
@@ -0,0 +1,13 @@
+name_list = ['TOM', 'Lily', 'ROSE']
+
+#1. index()
+# print(name_list.index('TOM'))
+# print(name_list.index('TOMs'))
+
+#2. count
+# print(name_list.count('TOM')) 
+# print(name_list.count('TOMS')) 
+
+#3.len()
+print(len(name_list))
+
