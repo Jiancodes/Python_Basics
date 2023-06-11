b7d6beac95a259a1aa987d8800a23ae9f140a815
jian899 <mjian528@gmail.com>
2023-06-11 20:21:39 +0800

    Add files via upload

---
+++ chapter_6_List/03_Determine_whether_there_is.py
@@ -0,0 +1,10 @@
+name_list = ['Tom' , 'Lily' , 'ROSE']
+# 1. in
+# print('Tom' in name_list)
+# print('Toms' in name_list)
+
+# 2. not in
+# print('Tom' not in name_list)
+print('Toms' not in name_list)
+
+
